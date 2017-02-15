bd3d40eb345b98f8e09e4c8ad1912cacb756c1a2
helladarion <helladarion@gmail.com>
2017-02-15 15:18:25 -0800

    Basic funcionality working with keyboard

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,5 +1,71 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+context.scale(20, 20);
+
+/** Creating the 7 pieces of tetris */
+const matrix = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0],
+];
+
+function draw() {
+  /** Clear the canvas every tick*/
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = 'red';
+        context.fillRect(x + offset.x,
+                         y + offset.y,
+                         1, 1);
+      }
+    });
+  });
+}
+
+function playerDrop() {
+  player.pos.y++;
+  dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+let lastTime = 0
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    playerDrop();
+  }
+
+  // console.log(deltaTime);
+  draw();
+  requestAnimationFrame(update);
+}
+
+const player = {
+  pos: {x: 5, y: 5},
+  matrix: matrix
+}
+
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 37) {
+    player.pos.x--;
+  } else if (event.keyCode === 39) {
+    player.pos.x++;
+  } else if (event.keyCode === 40) {
+    playerDrop();
+  }
+})
+
+update();
