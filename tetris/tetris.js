42ce04a37f6333a8062620220518e1a21318baf8
helladarion <helladarion@gmail.com>
2017-02-15 16:58:21 -0800

    Game finished with score and styles

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -3,12 +3,22 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-/** Creating the 7 pieces of tetris */
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0],
-];
+function arenaSweep() {
+  let rowCount = 1;
+  outer: for (let y = arena.length - 1; y > 0; --y) {
+    for (let x = 0; x < arena[y].length; ++x) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+    const row = arena.splice(y, 1)[0].fill(0); // remove area line fill it with zeros
+    arena.unshift(row); // put the line back in the top
+    ++y;
+
+    player.score += rowCount * 10;
+    rowCount *= 2;
+  }
+}
 
 function collide(arena, player) {
   const [m, o] = [player.matrix, player.pos];
@@ -30,6 +40,53 @@ function createMatrix(w, h) {
   return matrix;
 }
 
+/** Creating the 7 pieces of tetris */
+function createPiece(type) {
+  if (type === 'T') {
+    return  [
+              [0, 0, 0],
+              [1, 1, 1],
+              [0, 1, 0],
+            ];
+  } else if(type === 'O') {
+    return  [
+              [2, 2],
+              [2, 2],
+            ];
+  } else if(type === 'L') {
+    return  [
+              [0, 3, 0],
+              [0, 3, 0],
+              [0, 3, 3],
+            ];
+  } else if(type === 'J') {
+    return  [
+              [0, 4, 0],
+              [0, 4, 0],
+              [4, 4, 0],
+            ];
+  } else if(type === 'I') {
+    return  [
+              [0, 5, 0, 0],
+              [0, 5, 0, 0],
+              [0, 5, 0, 0],
+              [0, 5, 0, 0],
+            ];
+  } else if(type === 'S') {
+    return  [
+              [0, 6, 6],
+              [6, 6, 0],
+              [0, 0, 0],
+            ];
+  } else if(type === 'Z') {
+    return  [
+              [7, 7, 0],
+              [0, 7, 7],
+              [0, 0, 0],
+            ];
+  }
+}
+
 function draw() {
   /** Clear the canvas every tick*/
   context.fillStyle = '#000';
@@ -43,7 +100,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = 'red';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x,
                          y + offset.y,
                          1, 1);
@@ -67,7 +124,9 @@ function playerDrop() {
   if (collide(arena, player)) {
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset();
+    arenaSweep();
+    updateScore();
   }
   dropCounter = 0;
 }
@@ -76,6 +135,19 @@ function playerMove(dir) {
   player.pos.x += dir;
   if (collide(arena, player)) {
     player.pos.x -= dir;
+  }
+}
+
+function playerReset() {
+  const pieces = 'ILJOTSZ';
+  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+  player.pos.y = 0;
+  player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+  /** Game Over */
+  if(collide(arena, player)) {
+    arena.forEach(row => row.fill(0));
+    player.score = 0;
+    updateScore();
   }
 }
 
@@ -131,12 +203,30 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+function updateScore() {
+  document.getElementById('score').innerText = player.score;
+}
+
+const colors = [
+  null,
+  '#FF0D72',
+  '#0DC2FF',
+  '#0DFF72',
+  '#F538FF',
+  '#FF8E0D',
+  '#FFE138',
+  '#3877FF',
+];
+
+
+
 const arena = createMatrix(12, 20);
 
 
 const player = {
-  pos: {x: 5, y: 5},
-  matrix: matrix
+  pos: {x: 0, y: 0},
+  matrix: null,
+  score: 0,
 }
 
 document.addEventListener('keydown', event => {
@@ -153,4 +243,6 @@ document.addEventListener('keydown', event => {
   }
 })
 
+playerReset();
+updateScore();
 update();
